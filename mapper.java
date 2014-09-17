b8a4f580e81486552240d6448615b513c64ce340
ryosuke <ryosuke.inform@gmail.com>
2014-09-17 17:05:25 +0900

    方針を変えたので作り直しです。appendnodelistまで作れるようにしました。

--- mapper.java
+++ mapper.java
@@ -3,6 +3,7 @@ package ryosuke;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
@@ -16,6 +17,7 @@ public class mapper extends Mapper<LongWritable, Text, Text, IntWritable> {
 	public String FILENAME = "input/graph1.txt";
 	@Override
 	protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+		/*
 		String line = value.toString();
 		String[] words = line.split("\\s");
 		for (int i=0; i<words.length; i++)
@@ -25,8 +27,37 @@ public class mapper extends Mapper<LongWritable, Text, Text, IntWritable> {
 				context.write(new Text(words[i]), new IntWritable(1));
 			}
 		}
-		
+		*/
 		// hadoop以外の処理をここに記述
+		try{
+			FileReader in = new FileReader(FILENAME);
+			BufferedReader br = new BufferedReader(in);
+			String line = value.toString();
+			String[] splitcolon = line.split(":");
+			String nodenum = splitcolon[0];
+			//System.out.println("nodenum: " + nodenum);
+			
+			// reduce処理に渡すためのappendnodelistを作成する
+			String edgeset = splitcolon[1];
+			List<String> edge = Arrays.asList(edgeset.split("\t"));
+			Iterator<String> i = edge.iterator();
+			ArrayList appendnodelist = new ArrayList<String>();
+			while(i.hasNext()){
+				String[] splitcamma = i.next().split(",");
+				appendnodelist.add(splitcamma[1]);
+			}
+			//System.out.println("appendlist: " + appendnodelist.toString());
+			//System.out.println(Arrays.toString(edge));
+			//System.out.println(edgeset);
+			
+			while ((line = br.readLine()) != null) {
+				context.write(new Text(nodenum), new IntWritable(1));
+			}
+			
+		}catch (IOException e){
+			System.out.println(e);
+		}
+		/*
 		try {
 			FileReader in = new FileReader(FILENAME);
 			BufferedReader br = new BufferedReader(in);
@@ -57,6 +88,7 @@ public class mapper extends Mapper<LongWritable, Text, Text, IntWritable> {
 		} catch (IOException e) {
 			System.out.println(e);
 		}
+		*/
 	}
 	
 	// returnline: あるファイルの指定した行を返す
