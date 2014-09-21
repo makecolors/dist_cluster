5665cb79a3550191fdb4a0501e22b1f5397c7449
makecolors <ryosuke.inform@gmail.com>
2014-09-21 17:26:16 +0900

    リストの組み合わせまでは作り終わりました

--- reducer.java
+++ reducer.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -34,7 +35,7 @@ public class reducer extends Reducer<Text, Text, Text, IntWritable> {
 		String origin = key.toString();
 		
 		// 隣接するノードを調べる
-		List<String> adjnode = new ArrayList<String>();
+		LinkedList<String> adjnode = new LinkedList<String>();
 		List<Edge> otheredges = new ArrayList<Edge>();
 		Iterator<Edge> e = edges.iterator();
 		while(e.hasNext()){
@@ -49,21 +50,22 @@ public class reducer extends Reducer<Text, Text, Text, IntWritable> {
 				//System.out.printf("start: " + tempe.start + "end: " + tempe.end + "\n");
 			}
 		}
-		//System.out.println(adjnode);
+		System.out.println(adjnode);
 		//System.out.println(Arrays.asList(otheredges));
-		/*
-		Iterator<Edge> xx = otheredges.iterator();
-		while(xx.hasNext()){
-			Edge t = xx.next();
-			System.out.println(t.start + t.end);
-		}
-		*/
 		
-		/*
+		
 		// 隣接するノード同士の組み合わせを見つける
 		int trianglecount = 0;
 		List<Edge> a = combi(adjnode);
 		
+		Iterator<Edge> xx = a.iterator();
+		while(xx.hasNext()){
+			Edge t = xx.next();
+			System.out.println(t.start + t.end);
+		}
+		
+		
+		/*
 		// クラスタ係数の三角形をカウントする
 		Iterator<Edge> b = edges.iterator();
 		while(b.hasNext()){
@@ -78,16 +80,30 @@ public class reducer extends Reducer<Text, Text, Text, IntWritable> {
 		context.write(key, new IntWritable(trianglecount));
 		*/
 	}
-	private List<Edge> combi(List<String> adjnode) {
-		List<Edge> combi = null;
-		Iterator<String> i = adjnode.iterator();
+	private List<Edge> combi(LinkedList<String> adjnode) {
+		List<Edge> combi = new LinkedList<reducer.Edge>();
+		//Iterator<String> i = adjnode.iterator();
+		//System.out.println("=======start========");
+		//System.out.println(Arrays.asList(adjnode));
+		while(!adjnode.isEmpty()){
+			//System.out.printf("*");
+			String s = adjnode.pop();
+			for(String adj : adjnode){
+				combi.add(new Edge(s, adj));
+			}
+		}
+		//System.out.printf("\n");
+		/*
 		while(i.hasNext()){
+			String one = i.next();
 			Iterator<String> j = i;
 			while(j.hasNext()){
-				combi.add(new Edge(i.next(), j.next()));
+				//combi.add(new Edge(i.next(), j.next()));
+				System.out.println(one + j.next());
 			}
-			i.remove();
+			//i.remove();
 		}
+		*/
 		return combi;
 	}
 	class Edge{
